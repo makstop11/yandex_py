7a2581856908907abe4ac6f5ccea1253c6bdf392
Max Mic <spirit_vrs@mail.ru>
2024-04-12 16:08:48 +0300

    Циклы

--- chapter_2_4/F.py
+++ chapter_2_4/F.py
@@ -1,13 +1,9 @@
-# НОД - наибольший общий делитель
-
 a = int(input())
 b = int(input())
 
 while a != 0 and b != 0:
-
     if a > b:
         a = a % b
     else:
         b = b % a
-
 print(a + b)
