f3c10285234421d316c9a4c98f11161d325027f8
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-04-12 15:31:51 +0100

    Вложенные циклы
    - Пример задачи `J`

--- chapter_2_4/J.py
+++ chapter_2_4/J.py
@@ -1,5 +1,8 @@
 n = int(input())
 
-anya = 0
-borya = 0
-vova = 0
+print("А Б В")
+for anya in range(1, n - 1):
+    for borya in range(1, n - anya):
+        vova = n - anya - borya
+        if vova >= 1:
+            print(anya, borya, vova)
