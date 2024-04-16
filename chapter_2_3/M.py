068b0aebc855ab6c88cd93c55a20d7020618e68a
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-04-16 15:34:34 +0100

    Циклы
    - Нужно решить: `L`, `M`

---
+++ chapter_2_3/M.py
@@ -0,0 +1,13 @@
+players_count = int(input())
+
+names = []
+for _ in range(players_count):
+    player_name = input()
+    names.append(player_name)
+
+# TODO: Определить чье имя лексикографически меньше
+
+# TODO: Использовать цикл и найти это имя.
+
+# TODO: Использовать встроенную функцию
+#  sorted() или метод список.sort()
