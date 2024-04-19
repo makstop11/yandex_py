419a5175f8c1687fb643e905d02fa0d4ea71e1e3
Max Mic <spirit_vrs@mail.ru>
2024-04-19 17:01:16 +0300

    Циклы: задача I сделона

--- chapter_2_3/M.py
+++ chapter_2_3/M.py
@@ -4,7 +4,8 @@ names = []
 for _ in range(players_count):
     player_name = input()
     names.append(player_name)
-
+    player_name = names.sort(names)
+print(player_name)
 # TODO: Определить чье имя лексикографически меньше
 
 # TODO: Использовать цикл и найти это имя.
