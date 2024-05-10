32e32c9df8849a8418824a0379229726ade3d36f
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-05-10 14:10:45 +0100

    2.4 Вложенные циклы
    - Решение: `C`, `F`

--- chapter_2_4/C.py
+++ chapter_2_4/C.py
@@ -1,8 +1,13 @@
 n = int(input())
-count = 0
 
+count = 1
 for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        print(count + j, end=' ')
-    count += 1
+    for j in range(i):
+        if count > n:
+            break
+        print(count, end=' ')
+        count += 1
+
+    if count > n:
+        break
     print()
--- chapter_2_4/F.py
+++ chapter_2_4/F.py
@@ -1,11 +1,11 @@
 from math import gcd
 
-# nums = []
-# for _ in range(int(input())):
-#     nums.append(int(input()))
-#
-# curr_gcd = nums[0]
-# for i in range(1, len(nums)):
-#     curr_gcd = gcd(curr_gcd, nums[i])
-#
-# print(curr_gcd)
+nums = []
+for _ in range(int(input())):
+    nums.append(int(input()))
+
+curr_gcd = nums[0]
+for i in range(1, len(nums)):
+    curr_gcd = gcd(curr_gcd, nums[i])
+
+print(curr_gcd)
