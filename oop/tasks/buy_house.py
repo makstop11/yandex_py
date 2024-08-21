563c6581b9646cabb8aa460b6f381807b3778c4d
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-08-21 15:50:13 +0100

    Объектно-ориентированное программирование
    - ДЗ: "Покупка дома"
    - На доработку: "Алфавит"

--- oop/tasks/buy_house.py
+++ oop/tasks/buy_house.py
@@ -1,4 +1,3 @@
-# https://smartiqa.ru/courses/python/lesson-6#hometask
 
 
 class Human:
@@ -24,9 +23,48 @@ class Human:
     def __make_deal(self, house_obj, price):
         self.__money = self.__money - price
         self.__house = house_obj
+        print(f"Вы приобрели дом: {house_obj}")
 
     def earn_money(self, amount):
         self.__money = self.__money + amount
 
+    def buy_house(self, house, discount):
+        house_price = house.final_price(discount)
+        if self.__money < house_price:
+            print("Недостаточно средств")
+            return None
+        self.__make_deal(house, house_price)
+
+
+class House:
+    def __init__(self, area, price):
+        self._area = area
+        self._price = price
+
+    def final_price(self, sale):
+        discount_sum = (self._price / 100) * sale
+        result = self._price - discount_sum
+        return result
+
+
+class SmallHouse(House):
+    def __init__(self, price):
+        # House.__init__(self, area=40, price=price)
+        super().__init__(area=40, price=price)
+
 
 Human.default_info()
+vasya = Human("Вася", 19)
+# print(vasya.name)
+# print(vasya.age)
+# vasya.__house
+# vasya.__make_deal()
+
+vasya.info()
+
+small = SmallHouse(100)
+vasya.buy_house(small, 25)
+vasya.earn_money(500)
+vasya.buy_house(small, 25)
+
+vasya.info()
