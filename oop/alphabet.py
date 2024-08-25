c6525f209537202164df44c5bb393b9198d53b21
Max Mic <spirit_vrs@mail.ru>
2024-08-25 11:37:40 +0300

    ДЗ

--- oop/alphabet.py
+++ oop/alphabet.py
@@ -13,3 +13,18 @@ class Alphabet:
 class Tomato:
     states = ['зелёный','елёно-красный','красный']
     def __init__(self,_index,_stae):
+        pass
+    def is_ripe(self, grow):
+        grow[-1]
+class TomatoBush(Tomato):
+    def __init__(self, n:int):
+        s = []
+        pass
+    def grow_all(self):
+        s = []
+        pass
+    def all_are_ripe(self)
+        if s == grow[-1]:
+            return True
+    def give_away_all(sel):
+        del s
