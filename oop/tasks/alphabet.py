c6746d0ba2c0418eb144f15b71a2b29195debe00
Max Mic <spirit_vrs@mail.ru>
2024-08-25 11:41:41 +0300

    ДЗ

--- oop/tasks/alphabet.py
+++ oop/tasks/alphabet.py
@@ -1,4 +1,3 @@
-
 class Alphabet:
     def __init__(self, lang, letters):
         self.lang = lang
@@ -10,21 +9,29 @@ class Alphabet:
     def letters_num(self):
         return len(self.letters)
 
+
 class Tomato:
-    states = ['зелёный','елёно-красный','красный']
-    def __init__(self,_index,_stae):
+    states = ['зелёный', 'елёно-красный', 'красный']
+
+    def __init__(self, _index, _stae):
         pass
+
     def is_ripe(self, grow):
         grow[-1]
+
+
 class TomatoBush(Tomato):
-    def __init__(self, n:int):
+    def __init__(self, n: int):
         s = []
         pass
+
     def grow_all(self):
         s = []
         pass
+
     def all_are_ripe(self)
         if s == grow[-1]:
             return True
+
     def give_away_all(sel):
         del s
