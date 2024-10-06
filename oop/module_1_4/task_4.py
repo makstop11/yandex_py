37f2c781aa345d03f5923f1c233a313bfb6e01cc
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-10-06 10:27:37 +0100

    1.4 Методы классов. Параметр self
    - ДЗ: №4, №5, №7, №9 и №10
    
    - Правильное:
    - Неправильное: №4
    - На доработку: №4

--- oop/module_1_4/task_4.py
+++ oop/module_1_4/task_4.py
@@ -1,6 +1,13 @@
 class MediaPlayer:
-    pass
 
-open = MediaPlayer
+    def open(self, file: str):
+        self.filename = file
+
+        return self.filename
+
+    # TODO: доработать
 
 
+mp_1 = MediaPlayer()
+mp_1.open("video.mp4")
+print(mp_1.__dict__)
