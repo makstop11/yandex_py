986098bca52de86992531308d8c05a4ab575119b
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-12-08 09:26:23 +0000

    ООП | ДЗ
    - На доработку: "Покупка дома"
    
    - ДЗ:
        - Задача "Алфавит"

--- oop/smartiqa/ru/home.py
+++ oop/smartiqa/ru/home.py
@@ -1,14 +1,15 @@
 class Human:
-    defalt_name = ''
+    defalt_name = 'Неизвестно'
     default_age = 0
 
-    def __init__(self, name, age):
+    def __init__(self, name=defalt_name, age=default_age):
         self.name = name
         self.age = age
         self.__money = 0
-        self.__house = ''
+        self.__house = None
 
     def info(self):
+        # TODO: Улучшить вывод.
         print(self.name)
         print(self.age)
         print(self.__money)
@@ -19,6 +20,7 @@ class Human:
         print(Human.default_age)
         print(Human.defalt_name)
 
+    # TODO: Не соответствует ТЗ.
     def make_deal(self, house, price):
         self.__money -= price
         self.__house = house
@@ -26,6 +28,7 @@ class Human:
     def earn_money(self, money):
         self.__money += money
 
+    # TODO: Параметры метода: ссылка на дом и размер скидки
     def by_house(self):
         pass
 
@@ -35,5 +38,18 @@ class House:
         self._area = area
         self._price = price
 
-    def final_price(self, skidka):
-        self._price -= skidka
+    def final_price(self, discount):
+        # TODO: Неправильная реализация.
+        self._price = discount
+
+
+# TODO: SmallHouse
+
+if __name__ == '__main__':
+    # TODO: Добавить аннотация ко всем идентификатором.
+
+    h_1 = Human()
+
+    h_1.info()
+    h_1.earn_money(500)
+    h_1.info()
