d22c8a62caf38dcd906fde2b9a097383e1390c97
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-12-22 08:36:08 +0000

    Проверка задач по алгоритмам

--- practicum/algorithms/task2.py
+++ practicum/algorithms/task2.py
@@ -1,6 +1,10 @@
-oklad = input()
-procent = input()
+oklad = int(input())
+procent = int(input())
 
 nalog = oklad * procent / 100
 summa = oklad - nalog
-print(nalog, summa)
+
+print(
+    f"Ставка налога: {nalog}%"
+    f"\nПолучаемая на руки сумма: {summa}р"
+)
