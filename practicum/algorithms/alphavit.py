b452052a5ecfa7d716408a7a0420b5aeef6396da
Max Mic <spirit_vrs@mail.ru>
2025-01-05 11:33:54 +0300

    ДЗ

---
+++ practicum/algorithms/alphavit.py
@@ -0,0 +1,22 @@
+class Alphabet:
+    def __init__(self, leng, letters):
+        self.leng = leng
+        self.letters = letters
+
+    def print(self):
+        return self.letters
+
+
+def letters_num():
+    print(i for i in range(self.letters + 1))
+
+
+class EngAlphabet(Alphabet):
+    def __init__(self):
+        Alphabet.__init__(self, language)
+
+    def __letters_num(self, num):
+        self.num = num
+
+    def is_en_letter(self, b):
+        self.b = b
