96d04a4d2dce432759ebfe8f15b7189b93fbf4a8
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-01-05 09:21:44 +0000

    Разбор ДЗ | ООП
    - На доработку: "Алфавит"

--- practicum/algorithms/alphavit.py
+++ practicum/algorithms/alphavit.py
@@ -1,22 +1,36 @@
+import string
+
+
+# TODO: Нужны аннотации ко всем идентификаторам.
+
+
 class Alphabet:
     def __init__(self, leng, letters):
         self.leng = leng
         self.letters = letters
 
     def print(self):
-        return self.letters
+        print(self.letters)
 
-
-def letters_num():
-    print(i for i in range(self.letters + 1))
+    def letters_num(self):
+        return len(self.letters)
 
 
 class EngAlphabet(Alphabet):
+    __letters_num: int = 26
+
     def __init__(self):
-        Alphabet.__init__(self, language)
+        super().__init__("En", string.ascii_uppercase)
+        # Alphabet.__init__(self, lang, letters)
 
-    def __letters_num(self, num):
-        self.num = num
+    def is_en_letter(self, char: str) -> bool:
+        # return char in self.letters
 
-    def is_en_letter(self, b):
-        self.b = b
+        if char in self.letters:
+            return True
+        return False
+
+    def letters_num(self):
+        return self.__letters_num
+
+# TODO: Дорешать
