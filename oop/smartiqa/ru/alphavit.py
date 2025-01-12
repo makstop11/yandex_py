63340ec12dde19f7fbbbd6c1ce301fdc413fdc9e
Max Mic <spirit_vrs@mail.ru>
2025-01-12 11:31:15 +0300

    ДЗ

--- oop/smartiqa/ru/alphavit.py
+++ oop/smartiqa/ru/alphavit.py
@@ -1,25 +1,22 @@
 import string
 
 
-# TODO: Нужны аннотации ко всем идентификаторам.
-
-
 class Alphabet:
-    def __init__(self, leng, letters):
+    def __init__(self, leng, letters) -> None:
         self.leng = leng
         self.letters = letters
 
-    def print(self):
+    def print(self) -> None:
         print(self.letters)
 
-    def letters_num(self):
+    def letters_num(self) -> int:
         return len(self.letters)
 
 
 class EngAlphabet(Alphabet):
     __letters_num: int = 26
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("En", string.ascii_uppercase)
         # Alphabet.__init__(self, lang, letters)
 
@@ -30,7 +27,15 @@ class EngAlphabet(Alphabet):
             return True
         return False
 
-    def letters_num(self):
+    def letters_num(self) -> int:
         return self.__letters_num
 
-# TODO: Дорешать
+if __name__ == '__main__':
+    # vasya: Human = Human('Василий', 22)
+    # vasya.info()
+    # vasya.earn_money(1_500_000)
+    #
+    # h_1 = House(30, 1_000_000)
+    #
+    # vasya.buy_house(h_1, 0)
+    # vasya.info()
---
+++ practicum/algorithms/home_work.py
@@ -0,0 +1,39 @@
+# 2
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(min(a, b, c))
+print(max(a, b, c))
+
+# ----------------------------------------------------
+
+# 3
+
+import math
+
+d = int(input())
+f = int(input())
+g = int(input())
+h = (d + f + g) / 2
+
+print(a + b + c)
+print(math.sqrt(h * (h - d) * (h - f) * (h - g)))
+
+# ---------------------------------------------------
+
+j = int(input())
+
+t = j / 1000000
+k = j / 1000
+
+print(f'Тонны:{t}')
+print(f'Килограмм:{k}')
+
+# --------------------------------------------------
+
+l = int(input())
+
+n = l * 0,000977
+v = 
+
