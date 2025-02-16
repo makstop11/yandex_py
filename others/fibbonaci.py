2a5797865d3177d0283bcc6c727d8fd9464d74ad
Max Mic <spirit_vrs@mail.ru>
2025-02-16 13:08:50 +0300

    Доделал до 233

---
+++ others/fibbonaci.py
@@ -0,0 +1,63 @@
+n = int(input())
+
+x = 2
+y = 3
+
+
+while x < n:
+    print(x)
+    temp = y
+    y = x + y
+    x = temp
+
+
+# temp = 1
+# y = 1
+# x = 1
+
+# temp = 1
+# y = 2
+# x = 1
+
+
+# temp = 2
+# y = 3
+# x = 2
+
+
+# temp = 3
+# y = 5
+# x = 3
+
+
+# temp = 5
+# y = 8
+# x = 3
+
+# temp = 8
+# y = 13
+# x = 5
+
+# temp = 13
+# y = 21
+# x = 8
+
+# temp = 21
+# y = 34
+# x = 13
+
+# temp = 34
+# y = 55
+# x = 21
+
+# temp = 55
+# y = 89
+# x = 34
+
+# temp = 89
+# y = 144
+# x = 55
+
+# temp = 144
+# y = 233
+# x = 89
---
+++ practicum/homeworks/chapter_2/task_10.py
@@ -0,0 +1,4 @@
+def check_num(number: int) -> str:
+    if number % 2 != 0:
+        print('Невозможно разминять')
+#    elif number :
