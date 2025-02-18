0145a738c3fb261345d7a87c1058577fd6757d1c
Max Mic <spirit_vrs@mail.ru>
2025-02-18 20:40:25 +0300

    Так?

--- practicum/homeworks/chapter_2/task_10.py
+++ practicum/homeworks/chapter_2/task_10.py
@@ -1,4 +1,9 @@
-def check_num(number: int) -> str:
+def check_num(number: int) -> None:
     if number % 2 != 0:
         print('Невозможно разминять')
-#    elif number :
+    elif number % 500 == 0:
+        print('Купюр по 500: ', number / 500)
+    elif number % 100 == 0:
+        print('Купюр ро 100: ', number / 100)
+    elif number % 2 == 0:
+        print('Купюр ро 2: ', number / 2)
