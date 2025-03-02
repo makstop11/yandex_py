5f59580b65bea68d40a194559852b1591c397bc4
Max Mic <spirit_vrs@mail.ru>
2025-03-02 11:13:30 +0300

    алгоритмы поиска
    * реализация бинарного поиска

--- others/binary_search.py
+++ others/binary_search.py
@@ -1,22 +1,21 @@
-nums: list[int] = [12, 19, 21, 36, 47, 75, 77, 84, 92, 95, 97]
-target: int = 21
+def binary_search(nums: list[int], target: int) -> int:
+    length: int = len(nums)
 
-length: int = len(nums)
+    left: int = 0
+    right: int = length - 1
+    while left < right:
+        middle: int = (left + right) // 2
 
-left: int = 0
-right: int = length - 1
+        if target == nums[middle]:
+            return middle
 
-middle = (left + right) // 2
+        if target > nums[middle]:
+            left = middle + 1
+        elif target < nums[middle]:
+            right = middle - 1
 
-if target > nums[middle]:
-    left = middle + 1
-    middle = (right + left) // 2
-    left = middle + 1
-elif target < nums[middle]:
-    right = middle - 1
-    middle = (right + left) // 2
-    left = middle - 1
-else:
-    print(middle)
 
-print(middle)
+if __name__ == "__main__":
+    nums = [12, 19, 21, 36, 47, 75, 77, 84, 92, 95, 97]
+    target = 21
+    print(binary_search(nums, target))  # 2
