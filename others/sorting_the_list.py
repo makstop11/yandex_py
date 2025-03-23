cbe1485ef99429cf69c3f2059177db74a5734ef4
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-03-23 09:28:33 +0000

    Алгоритмы сортировки
    - Сортировка выбором

--- others/sorting_the_list.py
+++ others/sorting_the_list.py
@@ -1,11 +1,21 @@
-l: list[int] = [23, 42, 4, 16, 8, 15]
+nums: list[int] = [23, 42, 4, 16, 8, 15]
 
-lens = len(l)
+length: int = len(nums)
 
-for i in range(len(l) - 1):
-    m = 0
-    for j in range(i + 1):
-        if l[j] < l[m]:
-            m = j
-    if m != i:
-        ...
+for i in range(length):
+    curr_min: int = i
+    for j in range(i + 1, length):
+        if nums[j] < nums[curr_min]:
+            curr_min = j
+    if curr_min != i:
+        # a, b = b, a
+        nums[curr_min], nums[i] = nums[i], nums[curr_min]
+
+
+def selection_sort(arr: list[int]) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    selection_sort(nums)
+    print(nums)  # [4, 8, 15, 16, 23, 42]
